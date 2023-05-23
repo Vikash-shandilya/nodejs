81c762a2e03586bde38200cc6505e33830551ca7
Vikash-shandilya <vikashjha041@gmail.com>
2023-05-23 16:53:02 +0530

    rout various url

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
+  const url = req.url;
+  if (url === "/home") {
+    res.writeHead(200, { "content-type": "text/plain" });
+    res.write("welcome home");
+    return res.end();
+  } else if (url == "/about") {
+    res.writeHead(200, { "content-type": "text/plain" });
+    res.write("Welcome to About Us page");
+    return res.end();
+  } else if (url == "/node") {
+    res.writeHead(200, { "content-type": "text/plain" });
+    res.write(" Welcome to my Node Js project");
+    return res.end();
+  }
   res.writeHead(200, { "content-type": "text/plain" });
-  res.write("vikash kumar jha");
+  res.write("Welcome to my Node Js project");
   res.end();
 });
 
